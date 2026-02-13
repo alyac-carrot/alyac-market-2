318dbef1ea63d14cb43ae0a29243c7eaed301735
jsshin <jsshin0911@gmail.com>
2026-02-13 16:36:41 +0900

    feat: 라우터 적용

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,36 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-// Route Guard - 로그인 상태이면 피드 페이지로 리다이렉트 역할하는 컴포넌트
-import { RequireGuest } from '@/features/auth';
-import { FeedPage } from '@/pages/feed';
-// Pages
 import { HomePage } from '@/pages/home';
-import { SignInPage } from '@/pages/signin';
-
-import { RootLayout } from './layouts/RootLayout';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <RootLayout />,
+    // element: <RootLayout />,
     children: [
       {
         index: true,
         element: <HomePage />,
-      },
-      // 로그인이 필요 없는 페이지 (Guest만)
-      {
-        path: 'signin',
-        element: (
-          <RequireGuest>
-            <SignInPage />
-          </RequireGuest>
-        ),
-      },
-      // 보호된 페이지 (인증 필요)
-      {
-        path: 'feed',
-        element: <FeedPage />,
       },
     ],
   },
