8b00509050b96099374c27d153e04aa17411f96d
JunHwanJo <soldung12@gmail.com>
2026-02-19 16:40:00 +0900

    feat: svg 컴포넌트용 svgr 설정 추가

--- src/widgets/footer/ui/FooterNav.tsx
+++ src/widgets/footer/ui/FooterNav.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import { Home, MessageCircle, PlusSquare, User } from 'lucide-react';
+import { HomeIcon, ChatIcon, PostCreateIcon, ProfileIcon } from '@/assets/icon';
 
 export function Footer() {
   return (
@@ -7,22 +7,22 @@ export function Footer() {
       <nav className="mx-auto flex h-16 max-w-3xl items-center justify-around">
 
         <NavLink to="/feed" className="flex flex-col items-center">
-          <Home className="h-6 w-6" />
+          <HomeIcon className="h-6 w-6" />
           <span className="text-xs">홈</span>
         </NavLink>
 
         <NavLink to="/chat" className="flex flex-col items-center">
-          <MessageCircle className="h-6 w-6" />
+          <ChatIcon className="h-6 w-6" />
           <span className="text-xs">채팅</span>
         </NavLink>
 
         <NavLink to="/post-create" className="flex flex-col items-center">
-          <PlusSquare className="h-6 w-6" />
+          <PostCreateIcon className="h-6 w-6" />
           <span className="text-xs">게시물 작성</span>
         </NavLink>
 
         <NavLink to="/profile" className="flex flex-col items-center">
-          <User className="h-6 w-6 mb-1" />
+          <ProfileIcon className="h-6 w-6 mb-1" />
           <span className="text-xs ">프로필</span>
         </NavLink>
 
