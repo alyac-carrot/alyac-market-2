78732925066ae4bb5ca1bed4fead1ef0386af506
JunHwanJo <soldung12@gmail.com>
2026-02-19 14:47:25 +0900

    feat: 공통 Header 컴포넌트 추가

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
+import { RootLayout } from '@/app/layouts/RootLayout';
 
 import { HomePage } from '@/pages/home';
+
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    // element: <RootLayout />,
+    element: <RootLayout />,
     children: [
       {
         index: true,
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,5 @@
-import { Button } from '@/shared/ui';
-import { AlertDialog } from '@/shared/ui';
-
 export function HomePage() {
   return (
-    <div>Home
-    <Button>Click me</Button>
-    <AlertDialog>
-        <Button variant="outline">Open Alert Dialog</Button>
-         </AlertDialog>
-  </div>
+    <div>Home</div>
   )
 }
