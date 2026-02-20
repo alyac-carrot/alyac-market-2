f3f5e485bc87c2ca9522e719f78c6162c81f33db
고은표8-디버그 <goeunpyo8@gmail.com>
2026-02-20 11:43:21 +0900

    feat. 루트레이아웃 수정

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,15 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { RootLayout } from '@/app/layouts/RootLayout';
 
-import RootLayout from '@/app/layouts/RootLayout';
+import { RootLayout } from '@/app/layouts/RootLayout';
 import { HomePage } from '@/pages/home';
 import { SearchPage } from '@/pages/home/search';
-
-// TODO: 아래 3개는 페이지 만들고 추가
-// import { ChatPage } from "@/pages/chat";
-// import { WritePage } from "@/pages/write";
-// import { ProfilePage } from "@/pages/profile";
-
 
 export const router = createBrowserRouter([
   {
