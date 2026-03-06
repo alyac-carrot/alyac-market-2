5fb86637e8d7761fd1128ea00e90ac6649a91153
jsshin <jsshin0911@gmail.com>
2026-03-06 09:07:57 +0900

    feat: 회원가입 기능 추가

--- src/entities/auth/index.ts
+++ src/entities/auth/index.ts
@@ -1,8 +1,12 @@
 export { RequireAuth } from './lib/RequireAuth';
 export { useLogout } from './model/useLogout';
 export { useSignInMutation, getSignInErrorMessage } from './model/useSignInMutation';
+export { useSignUpMutation, getSignUpErrorMessage } from './model/useSignUpMutation';
 
 export { signIn } from './api/authApi';
 export type { SignInBody, SignInResponse } from './api/authApi';
+
+export { signUp, checkEmail, checkAccountname } from './api/signUpApi';
+export type { SignUpBody, SignUpResponse } from './api/signUpApi';
 
 export { getToken, removeToken, saveToken } from './lib/token';
