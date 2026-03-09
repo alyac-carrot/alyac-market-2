62aa62bdd43e748efa8102a237bc085505307cae
고은표8-디버그 <goeunpyo8@gmail.com>
2026-03-09 09:35:33 +0900

    feat. 스플래쉬 애니메이션 디자인 수정

--- src/pages/auth/splash/index.tsx
+++ src/pages/auth/splash/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 
 import { useNavigate } from 'react-router-dom';
+
 import { getToken } from '@/entities/auth';
 
 export default function SplashPage() {
@@ -24,7 +25,7 @@ export default function SplashPage() {
     <div className="flex min-h-screen items-center justify-center bg-white">
       <div className="flex flex-col items-center">
         {/* public/mascot.png */}
-        <img src="/mascot.png" alt="알약마켓" className="h-28 w-28 animate-bounce" />
+        <img src="/mascot.png" alt="알약마켓" className="h-28 w-28 animate-bounce object-contain" />
 
         <p className="mt-4 animate-bounce text-2xl font-extrabold text-green-600">알약마켓</p>
       </div>
